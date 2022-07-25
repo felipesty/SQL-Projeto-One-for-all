859f264a9c675414459c43ab0f176e3adc775dc0
felipesty <felipestylex08@gmail.com>
2022-07-25 14:10:00 -0300

    req 3: incluindo os Alias

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,6 @@
-SELECT t1.nome_usuario, COUNT(t2.id_musica), ROUND(SUM((t3.duracao_segundos)/60),2)
+SELECT t1.nome_usuario AS usuario,
+COUNT(t2.id_musica) AS qtde_musicas_ouvidas,
+ROUND(SUM((t3.duracao_segundos)/60),2) AS total_minutos
 	FROM SpotifyClone.usuarios AS t1
 	INNER JOIN SpotifyClone.historico AS t2
 	ON t1.id_usuario = t2.id_usuario
